015fd0c6f5ff3f4ee0e5b667a95a60283be93c43
ana-mariatambur <71002457+ana-mariatambur@users.noreply.github.com>
2020-12-08 15:27:43 +0200

    Add files via upload

---
+++ priblema1olimpiadaclasa9.py
@@ -0,0 +1,6 @@
+x=int(input("introduceti numarul de pasari:"))
+print(x,"pasari")
+g=x/2
+r=g/4
+t=x-g-r
+print(int(g),"gaini",int(r),"rate",int(t),"gasti")
---
+++ problema2olimpiadaclasa9.py
@@ -0,0 +1,4 @@
+n=int(input("introduceti numarul de alune:"))
+p=int(n/3)
+r=int(n-(p*3))
+print("pentru fiecare purcel revine",p,"alune,iar Ritei i-au ramas",r,"alune")
